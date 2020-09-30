d8ad2e1b0dc2286a734c8fc89b2eafe2bd2623bc
homi39 <misa.horiuchi0302@gmail.com>
2020-09-30 16:57:30 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,31 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+   has_many :kitchens
+   has_many :bathes
+   has_many :toilets
+   has_many :comments
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname
+    validates :email, uniqueness: true
+    validates :name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+    validates :name_kana, format: { with: /\A[ァ-ヶー－]+\z/}
+    validates :occupation_id
+    validates :gender_id
+    validates :family_member, format: { with: \d }
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
+    validates :birth
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :occupation_id
+    validates :gender_id
+  end
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :occupation_id
+  belongs_to_active_hash :gender_id
+
 end
