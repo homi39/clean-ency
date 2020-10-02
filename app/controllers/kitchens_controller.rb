722c689bbeafb0e139269a908c1f2dbc4211a5cb
homi39 <misa.horiuchi0302@gmail.com>
2020-10-02 09:38:47 +0900

    ビューの追加実装

--- app/controllers/kitchens_controller.rb
+++ app/controllers/kitchens_controller.rb
@@ -1,4 +1,5 @@
 class KitchensController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
 
   def index
     @kitchens = Kitchen.includes(:user).order("created_at DESC")
@@ -8,4 +9,11 @@ class KitchensController < ApplicationController
     @kitchen = Kitchen.find(params[:id])
   end
 
+  private
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to root_path
+    end
+  end
 end
--- app/controllers/toilets_controller.rb
+++ app/controllers/toilets_controller.rb
@@ -1,5 +1,6 @@
 class ToiletsController < ApplicationController
-
+  before_action :move_to_index, except: [:index, :show]
+  
   def index
     @toilets = Toilet.includes(:user).order("created_at DESC")
   end
@@ -8,4 +9,11 @@ class ToiletsController < ApplicationController
     @toilet = Toilet.find(params[:id])
   end
 
+  private
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to root_path
+    end
+  end
 end
